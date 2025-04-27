7b8e67ba43dd98ffec4b5c680227535e2047f6eb
Cameron Robinson <cameron@ckrobinson.net>
2025-04-26 18:38:35 -0700

    Update setupEnv. Add devEnv.sh

--- devEnv.sh
+++ devEnv.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
 
 script_dir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-filter=""
 dry="0"
-
-cd $script_dir
-scripts=$(find setupScripts -maxdepth 1 -mindepth 1 -perm +111 -type f)
 
 while [[ $# > 0 ]]; do
     if [[ "$1" == "--dry" ]]; then
         dry="1"
-    else
-        filter="$1"
     fi
+    shift
 done
 
 log() {
@@ -31,13 +26,24 @@ execute() {
     "$@"
 }
 
-log "run: filter=$filter"
+cd $scrip_dir
+copy_dir() {
+    pushd $1
+    to=$2
+    dirs=$(find . -maxdepth 1 -mindepth 1 -type d)
+    for dir in $dirs; do
+        execute rm -rf $to/$dir
+        execute cp -r $dir $to/dir
+    done
+}
 
-for script in $scripts; do
-    if echo "$script" | grep -qv "$filter"; then
-        log "Filtering: $filter -- $script"
-        continue
-    fi
-    log "Running script: $script"
-    execute ./$script
-done
+copy_file() {
+    from=$1
+    to=$2
+    name=$(basename $from)
+    execute rm $to/$name
+    execute cp $from $to/$name
+}
+
+# copy_dir .config $XDG_CONFIG_HOME
+
