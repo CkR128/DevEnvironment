e1e19afacf95b203f02f968f238548dc0ef57145
Cameron Robinson <cameron@ckrobinson.net>
2025-07-02 11:31:01 -0700

    Remove pylsp add pyright

--- .config/nvim/lua/plugins/nvim-lsp.lua
+++ .config/nvim/lua/plugins/nvim-lsp.lua
@@ -207,7 +207,8 @@ return {
 			-- clangd = {},
 			-- gopls = {},
 			-- pyright = {},
-			pylsp = {},
+			-- pylsp = {},
+			pyright = {},
 			-- rust_analyzer = {},
 			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 			--
