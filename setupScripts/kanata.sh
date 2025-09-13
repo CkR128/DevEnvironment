b6c30bfb4820986220b5555f0b28d6ac49ff4d6d
Cameron Robinson <cameron@ckrobinson.net>
2025-09-13 15:19:42 -0700

    Update kanata install script.

--- setupScripts/kanata.sh
+++ setupScripts/kanata.sh
@@ -1,2 +1,13 @@
 #!/bin/zsh
-brew install katana
+brew install --cask karabiner-elements
+echo "------------------------------------"
+echo "Run Karabiner from Applications and make sure to set up these permissions:"
+echo ""
+echo "Login Items & Extensions > Karabiner-Elements Non-Privileged Agents (enable)"
+echo "Login Items & Extensions > Karabiner-Elements Privileged Daemons (enable)"
+echo "Login Items & Extensions > Driver Extensions > .Karabiner-VirtualHIDDevice-Manager (enable)"
+echo "------------------------------------"
+brew install kanata
+echo "------------------------------------"
+echo "Run kanata with sudo and use --cfg path to kanatarc file."
+echo "------------------------------------"
