675838ebf85c500399cc1202704802c76291650c
Cameron Robinson <cameron@ckrobinson.net>
2025-09-12 17:30:20 -0700

    Unmap netrw tabs.

--- .config/nvim/lua/config/remap.lua
+++ .config/nvim/lua/config/remap.lua
@@ -97,3 +97,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "netrw",
+	callback = function()
+		vim.keymap.set("n", "t", "<Nop>", { buffer = true })
+	end,
+})
