f206839f72d762089f0d344499e6e246683c7906
Cameron Robinson <cameron@ckrobinson.net>
2025-10-14 12:16:02 -0700

    Update to install OhMyZSH and plugins.

--- setupScripts/powerlevel10k.sh
+++ setupScripts/powerlevel10k.sh
@@ -1,5 +1,10 @@
 #!/bin/zsh
 brew install powerlevel10k
+
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 echo ""
 echo "-------------------------------"
 echo "Run p10k configure to setup."
