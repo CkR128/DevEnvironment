4d73355950a3fe5c58daca975767d25d16030333
Cameron Robinson <cameron@ckrobinson.net>
2025-10-14 15:26:06 -0700

    Update brew to only install when needed.

--- setupScripts/1-brew.sh
+++ setupScripts/1-brew.sh
@@ -1,3 +1,9 @@
 #!/bin/zsh
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if [[ $(command -v brew) == "" ]]; then
+    echo "Installing Homebrew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Updating Homebrew"
+    brew update
+fi
 source ~/.zprofile
