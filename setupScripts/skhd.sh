d6c7bc8b8cc0a2c086132bcd68bf882a113e6f78
Cameron Robinson <cameron@ckrobinson.net>
2025-10-16 09:53:56 -0700

    Add Yabai and skhd for window tile management.

---
+++ setupScripts/skhd.sh
@@ -0,0 +1,7 @@
+brew install koekeishiya/formulae/skhd
+
+echo "------------------------------------"
+echo ""
+echo "See setup instructions here: https://github.com/koekeishiya/skhd"
+echo ""
+echo "------------------------------------"
---
+++ setupScripts/yabai.sh
@@ -0,0 +1,10 @@
+brew install koekeishiya/formulae/yabai 
+
+echo "------------------------------------"
+echo "See setup instructions here: https://github.com/koekeishiya/yabai/wiki/Installing-yabai-(latest-release)"
+echo ""
+echo "TL:DR - Start with yabai --start-service"
+echo "Allow accessability changes in System Preferences / Privacy & Security / Accessibility"
+echo "Unlock and allow changes to privacy list."
+echo ""
+echo "------------------------------------"
