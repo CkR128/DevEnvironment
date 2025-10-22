8640864817b3d972583bb606858b0512aeebef2e
Cameron Robinson <cameron@ckrobinson.net>
2025-10-21 17:04:58 -0700

    Update keybind lookup script.

--- .local/scripts/keybind-helper.sh
+++ .local/scripts/keybind-helper.sh
@@ -1,12 +1,17 @@
 #!/bin/zsh
 
-selected=`ls ~/.local/keybind-helper | fzf`
+selected=`ls $HOME/.local/keybind-helper/ | fzf`
 if [[ -z $selected ]]; then
     exit 0
 fi
 
 if [[ ! -f "$HOME/.local/keybind-helper/$selected" ]]; then
+    echo "Could not find file. Not sure how this happened."
     exit 1
 fi
 
-tmux neww bash -c "nvim $HOME/.local/keybind-helper/$selected"
+if [[ -z "$TMUX" ]]; then
+    tmux new-session -s keybind-lookup "bash -c 'man $HOME/.local/keybind-helper/$selected'" > /dev/null
+else
+    tmux neww -n keybind-lookup bash -c "man $HOME/.local/keybind-helper/$selected"
+fi
