e010f4fa1533673b54a15bd0366781e966c7491e
Cameron Robinson <cameron@ckrobinson.net>
2026-01-13 21:52:32 -0800

    Update setup scripts.

---
+++ setupScripts/ripgrep-rg.sh
@@ -0,0 +1,4 @@
+echo "download .tar from ripgrep github"
+echo "extract with tar xzvf"
+echo "run xattr -c on the rg program."
+echo "move to bin"
--- setupScripts/xcode-build-server.sh
+++ setupScripts/xcode-build-server.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
 INSTALL_LOCATION="$HOME/.local/"
 LINK_LOCATION="$HOME/.local/bin"
-git clone "https://github.com/SolaWing/xcode-build-server.git" "$INSTALL_LOCATION" && ln -s "$INSTALL_LOCATION"/xcode-build-server/xcode-build-server "$LINK_LOCATION"
+git clone "https://github.com/SolaWing/xcode-build-server.git" "$INSTALL_LOCATION"
+ln -s "$INSTALL_LOCATION"/xcode-build-server/xcode-build-server "$LINK_LOCATION"/xcode-build-server
