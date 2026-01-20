5e4c78f11dfbcb29483d28d9ae02623213fa9aa4
Cameron Robinson <cameron@ckrobinson.net>
2026-01-20 08:43:17 -0800

    Update sketchybar config.

--- .config/sketchybar/plugins/space.sh
+++ .config/sketchybar/plugins/space.sh
@@ -4,5 +4,19 @@
 # the space invoking this script (with name: $NAME) is currently selected:
 # https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
 
+if [[ $SELECTED == "false" ]]; then
+  if [[ "$NAME" =~ ^space\.([0-9]+)$ ]]; then
+    NUMBER="${BASH_REMATCH[1]}"
+    sketchybar --set "$NAME" \
+    icon="$NUMBER"
+  else
+    sketchybar --set "$NAME" \
+    icon="$NAME"
+  fi
+else 
 sketchybar --set "$NAME" \
-           background.drawing="$SELECTED"
+    icon=􀂓 \
+    # icon.padding_left=5 \
+    # icon.padding_right=5 \
+    # icon.color=0xffffffff
+fi
