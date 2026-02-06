3232b79dcf4718b1798d92e4af8203c40f8a199c
Cameron Robinson <cameron@ckrobinson.net>
2026-02-06 14:54:27 -0800

    Update keybind helper to run without tmux if not installed.

--- .local/scripts/keybind-helper.sh
+++ .local/scripts/keybind-helper.sh
@@ -10,6 +10,16 @@ if [[ ! -f "$HOME/.local/keybind-helper/$selected" ]]; then
     exit 1
 fi
 
+TMUX_INSTALLED=0
+if command -v tmux >/dev/null 2>&1; then
+    TMUX_INSTALLED=1
+fi
+
+if [[ TMUX_INSTALLED -eq 0 ]]; then
+    man $HOME/.local/keybind-helper/$selected
+    exit 0
+fi
+
 if [[ -z "$TMUX" ]]; then
     tmux new-session -s keybind-lookup "bash -c 'man $HOME/.local/keybind-helper/$selected'" > /dev/null
 else
