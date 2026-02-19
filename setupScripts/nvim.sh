31452c38d13ebabe0c4d57a992a7e5796dddb36a
Cameron Robinson <cameron@ckrobinson.net>
2026-02-19 08:16:01 -0800

    Update nvim install for initial install without brew.

--- setupScripts/nvim.sh
+++ setupScripts/nvim.sh
@@ -1,3 +1,16 @@
 #!/bin/zsh
-brew install tree-sitter-cli
-brew install neovim
+#
+if [[ $(command -v brew) == "" || $0 == 1 ]]; then
+    brew install tree-sitter-cli
+    brew install neovim
+else
+    # echo "Installing nvim from binary"
+    # curl -L -O "https://github.com/neovim/neovim/releases/download/v0.11.5/nvim-macos-arm64.tar.gz"
+    # tar -xzvf nvim-macos-arm64.tar.gz
+    # xattr -c nvim-macos-arm64/bin/nvim
+    # chmod u+x nvim-macos-arm64/bin/nvim
+    # mkdir -p $HOME/.bin/
+    # mv nvim-macos-arm64/bin/nvim $HOME/.bin/nvim
+    # rm -frd ./nvim-macos-arm64/
+    # rm nvim-macos-arm64.tar.gz
+fi
