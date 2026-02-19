fc16631b50da95027c89da2d86079ab79d072c68
Cameron Robinson <cameron@ckrobinson.net>
2026-02-19 13:07:08 -0800

    Update info in nvim brew-less install.

--- setupScripts/nvim.sh
+++ setupScripts/nvim.sh
@@ -13,4 +13,8 @@ else
     # mv nvim-macos-arm64/bin/nvim $HOME/.bin/nvim
     # rm -frd ./nvim-macos-arm64/
     # rm nvim-macos-arm64.tar.gz
+    echo "Add the following to your .zprofile to change the nvim runtime directory if needed:"
+    echo "export VIMRUNTIME=\"\$HOME/.bin/nvim-config/share/nvim/runtime\""
+    echo "You may need to copy over the directory from the tar to that file path, or another file path of your choice. Should be bin/ lib/ and share/ in the path, nvim/ under lib/ and icons/ man/ and nvim/ under share/"
+    echo "OR this may be create on first launch?"
 fi
