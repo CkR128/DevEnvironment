63f231d435783c368751c98aa55b802f0b884be1
Cameron Robinson <cameron@ckrobinson.net>
2026-02-20 21:13:08 -0800

    Update nvim install without brew.

--- setupScripts/nvim.sh
+++ setupScripts/nvim.sh
@@ -1,20 +1,20 @@
 #!/bin/zsh
 #
+INSTALL_PATH="$HOME/.bin"
 if [[ $(command -v brew) == "" || $0 == 1 ]]; then
     brew install tree-sitter-cli
     brew install neovim
 else
-    # echo "Installing nvim from binary"
-    # curl -L -O "https://github.com/neovim/neovim/releases/download/v0.11.5/nvim-macos-arm64.tar.gz"
-    # tar -xzvf nvim-macos-arm64.tar.gz
-    # xattr -c nvim-macos-arm64/bin/nvim
-    # chmod u+x nvim-macos-arm64/bin/nvim
-    # mkdir -p $HOME/.bin/
-    # mv nvim-macos-arm64/bin/nvim $HOME/.bin/nvim
-    # rm -frd ./nvim-macos-arm64/
-    # rm nvim-macos-arm64.tar.gz
-    echo "Add the following to your .zprofile to change the nvim runtime directory if needed:"
+    echo "Installing nvim from binary"
+    curl -L -O "https://github.com/neovim/neovim/releases/download/v0.11.5/nvim-macos-arm64.tar.gz"
+    tar -xzvf nvim-macos-arm64.tar.gz
+    xattr -c nvim-macos-arm64/bin/nvim
+    chmod u+x nvim-macos-arm64/bin/nvim
+    mkdir -p "$INSTALL_PATH" 
+    mv nvim-macos-arm64/ "$INSTALL_PATH/nvim-config/"
+    ln "$INSTALL_PATH/nvim-config/bin/nvim" "$INSTALL_PATH/nvim"
+    rm nvim-macos-arm64.tar.gz
+    echo "Add the following to your .zprofile to change the nvim runtime directory:"
     echo "export VIMRUNTIME=\"\$HOME/.bin/nvim-config/share/nvim/runtime\""
-    echo "You may need to copy over the directory from the tar to that file path, or another file path of your choice. Should be bin/ lib/ and share/ in the path, nvim/ under lib/ and icons/ man/ and nvim/ under share/"
-    echo "OR this may be create on first launch?"
+    echo "export PATH=\"\$PATH:$HOME/.bin/\""
 fi
